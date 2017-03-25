a73e4791f40f573cf4863b8d1fabe716a9e48a19
Fredric Bergstrom <fredric.bergstrom@wunderkraut.se>
2017-03-25 15:35:49 +0100

    add my tests

---
+++ web/modules/custom/ddd/tests/src/Functional/BrowserTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\Tests\ddd\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Example functional test.
+ *
+ * @group ddd
+ */
+class BrowserTest extends BrowserTestBase {
+
+  protected $user;
+
+  public static $modules = ['block', 'node', 'datetime'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->drupalCreateContentType([ 'type' => 'page', 'name' => 'Basic page']);
+    $this->user = $this->drupalCreateUser([ 'edit own page content', 'create page content']);
+    $this->drupalPlaceBlock( 'local_tasks_block');
+  }
+
+
+  function testNodeCreate() {
+    $this->drupalLogin($this->user);
+    $title = $this->randomString();
+    $body = $this->randomString( 32);
+    $edit = [
+      'Title' => $title,
+      'Body' => $body,
+    ];
+    $this->drupalPostForm( 'node/add/page', $edit, t('Save'));
+
+    // Test that the title is the same
+    $node = $this->drupalGetNodeByTitle($title);
+    $this->assertTrue($node);
+    $this->assertEquals($title, $node->getTitle());
+    $this->assertEquals($body, $node->body->value);
+  }
+
+
+  function _testDrupalGet() {
+    $this->drupalGet('user/register');
+    $this->assertSession()->pageTextContains( 'Create new account');
+    $this->assertSession()->fieldExists( 'Email address');
+    $this->assertSession()->fieldExists( 'Username');
+    $this->assertSession()->buttonExists( 'Create new account');
+    $this->assertSession()->pageTextNotContains( 'Joomla');
+  }
+}
